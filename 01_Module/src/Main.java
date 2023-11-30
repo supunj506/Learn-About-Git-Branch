3adaa479f388448ae4428914a6e7b01f88147cfd
supunj506 <supunj506@gmail.com>
2023-11-30 13:51:00 +0530

    First commit

---
+++ 01_Module/src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Author  : supun@mj
+ * Project : ProjectTestBranch
+ * Date    : 11/30/2023
+ * Time    : 1:49 PM
+ * For GDSE course of IJSE Institute.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("run");
+    }
+}
